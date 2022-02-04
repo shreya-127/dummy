bbe3a69fc87e4d48f1419bf19875746bf1c1054e
shreya <shreya@DESKTOP-3BA5THE>
2022-02-04 20:48:40 +0530

    First Commit- Dummy

---
+++ Dummy/src/pkg1/dummyclass.java
@@ -0,0 +1,23 @@
+package pkg1;
+
+public class dummyclass {
+
+
+
+		int a;
+		public void Shreya()
+		{
+			System.out.println("New class");
+		}
+		public static void main(String[] args)
+		{
+			dummyclass abc=new dummyclass();
+			abc.Shreya();
+			abc.a=22;
+			System.out.println(abc.a);
+			abc.a=34;
+			System.out.println(abc.a);
+		}
+	}
+
+
